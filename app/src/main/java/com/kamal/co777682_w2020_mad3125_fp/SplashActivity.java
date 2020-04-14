bde9b37e54018d3a3b2e35c94fa218123166a20b
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-04-14 11:55:46 -0400

    Splash acitivity added

---
+++ app/src/main/java/com/kamal/co777682_w2020_mad3125_fp/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.kamal.co777682_w2020_mad3125_fp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
