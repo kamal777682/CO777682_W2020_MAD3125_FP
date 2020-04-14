93fe747a2ef0272262e7f8cffb21f3d149e25432
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-04-14 12:04:13 -0400

    Splash Screen working

--- app/src/main/java/com/kamal/co777682_w2020_mad3125_fp/SplashActivity.java
+++ app/src/main/java/com/kamal/co777682_w2020_mad3125_fp/SplashActivity.java
@@ -2,13 +2,30 @@ package com.kamal.co777682_w2020_mad3125_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.kamal.co777682_w2020_mad3125_fp.LoginActivity;
+import com.kamal.co777682_w2020_mad3125_fp.R;
 
 public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        this.setTitle("");
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(mIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
