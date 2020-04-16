491a8b50f50c8fe62904650746b796379ad855d2
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-04-16 18:27:03 -0400

    [kamal] Validations working

--- app/src/main/java/com/kamal/co777682_w2020_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/kamal/co777682_w2020_mad3125_fp/LoginActivity.java
@@ -1,8 +1,8 @@
 package com.kamal.co777682_w2020_mad3125_fp;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -18,24 +17,27 @@ public class LoginActivity extends AppCompatActivity {
 
     private TextInputEditText email;
     private TextInputEditText password;
-    private Button login;
+    private  Button btnlogin;
     private CheckBox rememberMe;
     private SharedPreferences myPref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
-        login = findViewById(R.id.login);
-        myPref = getSharedPreferences("login",MODE_PRIVATE);
+        email = findViewById(R.id.txtemail);
+        password = findViewById(R.id.txtpassword);
+        btnlogin = findViewById(R.id.login);
+        rememberMe = findViewById(R.id.rememberMe);
+        myPref = getSharedPreferences("MyPref",MODE_PRIVATE);
+
+        myPref = getSharedPreferences("Login",MODE_PRIVATE);
         //https://stackoverflow.com/questions/2154438/how-do-i-implement-a-remember-me-function-in-an-android-activity
         final SharedPreferences.Editor editor  = myPref.edit();
 
         email.setText(myPref.getString("email",null));
         password.setText(myPref.getString("password",null));
 
-        login.setOnClickListener(new View.OnClickListener() {
+        btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(email.getText().toString().equals("kamal1234@gmail.com") && password.getText().toString().equals("kamal")){
@@ -59,6 +61,7 @@ public class LoginActivity extends AppCompatActivity {
                 else {
                     AlertDialog.Builder alert = new AlertDialog.Builder(LoginActivity.this);
                     alert.setCancelable(false);
+                    alert.setTitle("Login Error");
                     alert.setMessage("Invalid User ID or Password");
                     alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         @Override
@@ -78,4 +81,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
