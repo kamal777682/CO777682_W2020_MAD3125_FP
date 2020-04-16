9650acc21aefd01bfa207c03eea1c8d2b8e66e04
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-04-16 19:09:22 -0400

    [kamal] Bill Activities created

---
+++ app/src/main/java/com/kamal/co777682_w2020_mad3125_fp/UI/AddInternetBillActivity.java
@@ -0,0 +1,16 @@
+package com.kamal.co777682_w2020_mad3125_fp.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kamal.co777682_w2020_mad3125_fp.R;
+
+public class AddInternetBillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_internet_bill);
+    }
+}
