377001eacc2dba96ddbc47b6e6f0bdbee247ac49
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-04-18 20:04:17 -0400

    [kamal] date picker added

--- app/src/main/java/com/kamal/co777682_w2020_mad3125_fp/UI/AddInternetBillActivity.java
+++ app/src/main/java/com/kamal/co777682_w2020_mad3125_fp/UI/AddInternetBillActivity.java
@@ -3,9 +3,14 @@ package com.kamal.co777682_w2020_mad3125_fp.UI;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
 
 import com.kamal.co777682_w2020_mad3125_fp.R;
+
+import java.util.Calendar;
 
 public class AddInternetBillActivity extends AppCompatActivity {
 
@@ -15,5 +20,28 @@ public class AddInternetBillActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_internet_bill);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Add Internet Bill");
+        findViewById(R.id.btnDatePicker).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
     }
+    private void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this, this,
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH),
+                Calendar.getInstance().get(Calendar.WEEK_OF_MONTH),
+                Calendar.getInstance().get(Calendar.YEAR));
+        datePickerDialog.show();
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String date =  dayOfMonth + "/" + month+1 + "/" + year;
+        billDate.setText(date);
+    }
+
+}
 }
