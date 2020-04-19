0c07a697e52fe19cca9afbd8decc06a409f1ed06
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-04-18 20:30:15 -0400

    [kamal] Date picker added in all Activities

--- app/src/main/java/com/kamal/co777682_w2020_mad3125_fp/UI/AddMobileBillActivity.java
+++ app/src/main/java/com/kamal/co777682_w2020_mad3125_fp/UI/AddMobileBillActivity.java
@@ -3,17 +3,67 @@ package com.kamal.co777682_w2020_mad3125_fp.UI;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.kamal.co777682_w2020_mad3125_fp.R;
 
-public class AddMobileBillActivity extends AppCompatActivity {
+import java.util.Calendar;
 
+public class AddMobileBillActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    private TextInputEditText billId;
+    private TextInputEditText billDate;
+    private TextInputEditText billType;
+    private TextInputEditText manufacturerName;
+    private TextInputEditText mobilePlan;
+    private TextInputEditText mobileNumber;
+    private TextInputEditText billAmount;
+    private TextInputEditText internetGBUsed;
+    private TextInputEditText minuteUsed;
+    private Button btnSave;
+    private Button btnCancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_mobile_bill);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Add Mobile Bill");
+        billId = findViewById(R.id.txtBillID);
+        billDate = findViewById(R.id.txtBillDate);
+        billType = findViewById(R.id.txtBillType);
+        manufacturerName = findViewById(R.id.txtManufactureName);
+        mobilePlan = findViewById(R.id.txtMobileplan);
+        mobileNumber = findViewById(R.id.txtMobileNumber);
+        billAmount = findViewById(R.id.txtBillAmount);
+        internetGBUsed = findViewById(R.id.txtGBUsed);
+        minuteUsed = findViewById(R.id.txtMinuteUsed);
+        btnSave = findViewById(R.id.btnSave);
+        btnCancel = findViewById(R.id.btnCancel);
+        findViewById(R.id.btnDatePicker).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+    }
+    private void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this, this,
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH),
+                Calendar.getInstance().get(Calendar.WEEK_OF_MONTH),
+                Calendar.getInstance().get(Calendar.YEAR));
+        datePickerDialog.show();
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String date =  dayOfMonth + "/" + month+1 + "/" + year;
+        billDate.setText(date);
     }
 }
