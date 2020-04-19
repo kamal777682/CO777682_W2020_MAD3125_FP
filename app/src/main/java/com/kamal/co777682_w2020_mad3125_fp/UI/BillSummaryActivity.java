7adbbdbac031fc75e29b8b080096c07b32cf9818
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-04-19 15:00:26 -0400

    [kamal]  working on bill summary

--- app/src/main/java/com/kamal/co777682_w2020_mad3125_fp/UI/BillSummaryActivity.java
+++ app/src/main/java/com/kamal/co777682_w2020_mad3125_fp/UI/BillSummaryActivity.java
@@ -3,11 +3,25 @@ package com.kamal.co777682_w2020_mad3125_fp.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.kamal.co777682_w2020_mad3125_fp.R;
 
 public class BillSummaryActivity extends AppCompatActivity {
 
+    private TextView billId;
+    private  TextView billDate;
+    private TextView billType;
+    private TextView billAmount;
+    private TextView agencyName;
+    private TextView unitConsumed;
+    private TextView internetProvider;
+    private TextView internetUsage;
+    private TextView manufactureName;
+    private TextView mobilePlan;
+    private TextView mobileNumber;
+    private TextView mobileInternetused;
+    private TextView minuteUsed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
