7ab5317e36372099e30f88fdad2eb803631f231c
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-04-19 22:44:39 -0400

    [kamal] changes made

--- app/src/main/java/com/kamal/co777682_w2020_mad3125_fp/UI/AddInternetBillActivity.java
+++ app/src/main/java/com/kamal/co777682_w2020_mad3125_fp/UI/AddInternetBillActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.kamal.co777682_w2020_mad3125_fp.Models.Customers;
+import com.kamal.co777682_w2020_mad3125_fp.Models.HydroBill;
+import com.kamal.co777682_w2020_mad3125_fp.Models.InternetBill;
 import com.kamal.co777682_w2020_mad3125_fp.R;
 
 import java.util.Calendar;
@@ -22,6 +26,7 @@ public class AddInternetBillActivity extends AppCompatActivity implements DatePi
     private TextInputEditText billAmount;
     private Button btnSave;
     private Button btnCancel;
+    public  static   Customers customer1;
 
 
     @Override
@@ -35,6 +40,42 @@ public class AddInternetBillActivity extends AppCompatActivity implements DatePi
         internetProvider = findViewById(R.id.txtInternetProvider);
         internetGBUsed = findViewById(R.id.txtGBUsed);
         btnSave = findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String billid = billId.getText().toString().trim();
+                String billdate = billDate.getText().toString().trim();
+                String billamount = billAmount.getText().toString().trim();
+                String intP = internetProvider.getText().toString().trim();
+                String usage = internetGBUsed.getText().toString().trim();
+                if(billid.isEmpty())
+                {
+                    billId.setError("Enter Bill Id");
+                }
+                if(billdate.isEmpty())
+                {
+                    billDate.setError("Enter Bill Date");
+                }
+                if(billamount.isEmpty())
+                {
+                    billAmount.setError("Enter Bill Amount");
+                } if(intP.isEmpty())
+                {
+                    internetProvider.setError("Enter the Provider Name");
+                } if(usage.isEmpty())
+                {
+                    internetGBUsed.setError("Enter the Internet Used(GB)");
+                }
+                else
+                {
+                    InternetBill tempInternet = new InternetBill(billid,billdate,Double.parseDouble(billamount),intP, Integer.parseInt(usage));
+                    customer1.addBill(tempInternet.getBillId(), tempInternet);
+                    Intent intent = new Intent(AddInternetBillActivity.this,ShowBillDetailsActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
         findViewById(R.id.btnDatePicker).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
